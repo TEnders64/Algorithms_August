a551e2841d4feb7149e38fb7fd0f00c45993e143
Nicholis Muller <nicholis.muller@gmail.com>
2016-08-08 21:46:28 +0200

    Update console.log in Monday Algorithm

--- Nicholis_Muller/Algorithms/14_Monday_Algorithims_8Aug.js
+++ Nicholis_Muller/Algorithms/14_Monday_Algorithims_8Aug.js
@@ -18,9 +18,9 @@ function remNeg(arr){
     for (var i = 0; i < arr.length; i++) {
         if(arr[i] < 0){
             delete(arr[i]);
-            console.log(arr);
         }
     }
+    console.log(arr);
 }
 
 revArray([-3, -2, -1, 0, 2, 3, 4, 5, 6, 7]);
