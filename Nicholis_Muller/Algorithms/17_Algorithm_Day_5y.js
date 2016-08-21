5dc6e33719634127241d872df11db1ddfd42027e
Nicholis Muller <Nicholis Muller>
2016-08-22 01:57:45 +0200

    Day 5 update

--- Nicholis_Muller/Algorithms/17_Algorithm_Day_5y.js
+++ Nicholis_Muller/Algorithms/17_Algorithm_Day_5y.js
@@ -1,8 +1,39 @@
 // Shuffle
 // Recreate the shuffle(arr) function built into JavaScript, to efficiently shuffle a given array’s values. Work in-place, of course. Do you need to return anything from your function?
 
+function shuffle(arr){
+
+    for (var i = arr.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        //no need to return as it is implicit.
+}
+
+//shuffle([1, 3, 5, 7, 23, 55]);
+
 // Remove from array
 // Given array, and indices start and end, remove vals in that index range, working in-place (hence shortening the array). Given ([20,30,40,50,60,70],2,4), change to [20,30,70] and return it.
+
+function removeRange(arr, start, end) {
+var counter = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        while (arr[i] === start || arr[i] === end )
+        console.log(arr[i]);
+
+    }
+
+
+
+}
+
+
+removeRange([3, 2, 1, 4, 5], 2, 4 );
+
 
 // Intermediate Sums
 // You will be given an array of numbers. After every tenth element, add an additional element containing the sum of those ten values. If the array does not end aligned evenly with ten elements, add one last sum that includes those last elements not yet been included in one of the earlier sums. Given the array [1,2,1,2,1,2,1,2,1,2,1,2,1,2], change it to [1,2,1,2,1,2,1,2,1,2,15,1,2,1,2,6].
