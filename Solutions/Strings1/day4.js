fb43594aa20e036bbb68fd1abe08c55390303529
Michael Arbogast <marboga@gmail.com>
2016-08-23 13:47:23 -0700

    added slice and concat to day 4

--- Solutions/Strings1/day4.js
+++ Solutions/Strings1/day4.js
@@ -2,22 +2,55 @@
 //
 // string1.concat(str2,str3,...,strX) - add string(s) to end of existing one. Return new string.
 //
+String.prototype.myConcat = function(strings) {
+	var newString = this;
+	// console.log(this)
+	for (var i = 0; i < arguments.length; i++) {
+		newString += arguments[i];
+	}
+	return newString;
+}
+var myString = "helloworld"
+console.log(myString.myConcat("hello", "world"));
 
 // String.slice
 //
 // string.slice(start,end) - extract part of a string and return in a new one. Start and end are indices into the string, with the first character at index 0. End param is optional and, if present, refers to one beyond the last character to include.
 //
 // Bonus: include support for negative indices, representing offsets from string-end. Example: string.slice(-1) returns the string’s last character.
+String.prototype.mySlice = function(start, end) {
+	var newString = ""
+	var string = this;
+	if (end < start) { return this; }
+	if (end > string.length) {
+		end = string.length;
+	}
+	console.log(string.length)
+	for (var i = 0; i < string.length; i++) {
+		if (i >= start && i <= end) {
+			console.log(string[i])
+			// break;
+		} else {
+			newString += string[i]
+		}
+	}
+	return newString;
+}
+console.log(myString.mySlice(2,4));
 // String.trim
 //
 // string.trim() - remove whitespace (spaces, tabs, newlines) from both sides, and return a new string.
 //
 // Example: "\n hellogoodbye\t ".trim() should return "hellogoodbye".
+
+
 // String.split
 //
 // string.split(separator,limit) - split string into array of substrings, returning the new array. Separator specifies where to divide substrings and is not included in any substring. If "" is specified, split string on every character. Limit is optional and indicates number of splits; additional post-limit items should be discarded.
 //
 // Note: existing string is unaffected.
+
+
 // String.search
 //
 // string.search(val) - search string for val (another string). Return index position of first match (-1 if not found).
---
+++ Solutions/Strings1/day5.js
@@ -0,0 +1,7 @@
+
+// Book Index
+// Martin is writing his opus: a book of algorithm challenges, set as lyrics to a suite of baroque fugues. He knows that some of those fugueing challenges will be less popular than others, so he needs a book index. Given a sorted array of pages, produce a book index string. Consecutive pages should form ranges separated by a dash. For [1,3,4,5,7,8,10], return the string "1, 3-5, 7-8, 10".
+
+
+// Common Suffix
+// Lance is writing his opus: a tome of beat poetry. Always looking for a good rhyme, he seeks words ending with the same letters. Write a function that, given a word array, returns the largest common suffix (word-end). For inputs ["deforestation", "citation", "conviction", "incarceration"], return "tion" (not a very creative starting point). If it is ["nice", "ice", "baby"], return "".
