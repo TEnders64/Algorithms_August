fd82843cd72c1c2aa90faf920c0966fc03c5c7e0
Michael Arbogast <marboga@gmail.com>
2016-08-23 17:55:08 -0700

    added bookIndex algorithm

--- Solutions/Strings1/day4.js
+++ Solutions/Strings1/day4.js
@@ -10,7 +10,7 @@ String.prototype.myConcat = function(strings) {
 	}
 	return newString;
 }
-var myString = "helloworld"
+var myString = "   	helloworld 		\n"
 console.log(myString.myConcat("hello", "world"));
 
 // String.slice
@@ -21,14 +21,15 @@ console.log(myString.myConcat("hello", "world"));
 String.prototype.mySlice = function(start, end) {
 	var newString = ""
 	var string = this;
-	if (end < start) { return this; }
+	if (!end) { end = string.length; }
+	// if (end < start) { return this; }
 	if (end > string.length) {
 		end = string.length;
 	}
-	console.log(string.length)
+	// console.log(string.length)
 	for (var i = 0; i < string.length; i++) {
 		if (i >= start && i <= end) {
-			console.log(string[i])
+			// console.log(string[i])
 			// break;
 		} else {
 			newString += string[i]
@@ -36,21 +37,81 @@ String.prototype.mySlice = function(start, end) {
 	}
 	return newString;
 }
-console.log(myString.mySlice(2,4));
+// console.log(myString.mySlice(2,4));
 // String.trim
 //
 // string.trim() - remove whitespace (spaces, tabs, newlines) from both sides, and return a new string.
 //
 // Example: "\n hellogoodbye\t ".trim() should return "hellogoodbye".
-
+String.prototype.myTrim = function () {
+	var newString = "";
+	var oldString = this;
+	var spaces = ['\s', '\t', '\n', ' ', '&nbsp;']
+	for (var i = 0; i <= oldString.length; i++) {
+		if (spaces.indexOf(oldString[i]) != -1) {
+			// console.log('space')
+		} else {
+			newString = oldString.mySlice(0, i-1)
+			// console.log(oldString);
+			break
+		}
+	}
+	for (var i = newString.length; i >= 0; i--) {
+		if (spaces.indexOf(newString[i]) != -1) {
+			// console.log('space')
+		} else {
+			newString = newString.mySlice(i)
+			// console.log(newString);
+			break
+		}
+	}
+	return newString;
+};
+console.log(myString.myTrim())
 
 // String.split
 //
 // string.split(separator,limit) - split string into array of substrings, returning the new array. Separator specifies where to divide substrings and is not included in any substring. If "" is specified, split string on every character. Limit is optional and indicates number of splits; additional post-limit items should be discarded.
 //
 // Note: existing string is unaffected.
-
+String.prototype.mySplit = function(separator, limit) {
+	var resultArray = [];
+	var begin = 0;
+	if (!limit) {
+		limit = this.length;
+	}
+	if (separator !== ''){
+		for (var i = 0; i < this.length; i++) {
+			if (this[i] === separator) {
+				resultArray.push(this.slice(begin,i));
+				begin = i;
+			}
+		}
+	} else {
+		for (var i = 0; i < this.length; i++) {
+			resultArray.push(this[i])
+		}
+	}
+	return resultArray;
+}
+console.log(myString.mySplit(' '))
 
 // String.search
 //
 // string.search(val) - search string for val (another string). Return index position of first match (-1 if not found).
+String.prototype.mySearch = function(val) {
+	var string = this;
+	console.log(val[0])
+	for (var i = 0; i < string.length; i++) {
+		if (string[i] === val[0]) {
+			for (var j = 0; j < val.length; j++) {
+				if (val[j] !== string[i + j]) {
+					return -1;
+				}
+			}
+			return i;
+		}
+	}
+	return -1
+}
+console.log(myString.mySearch('hel'))
--- Solutions/Strings1/day5.js
+++ Solutions/Strings1/day5.js
@@ -1,7 +1,44 @@
 
 // Book Index
 // Martin is writing his opus: a book of algorithm challenges, set as lyrics to a suite of baroque fugues. He knows that some of those fugueing challenges will be less popular than others, so he needs a book index. Given a sorted array of pages, produce a book index string. Consecutive pages should form ranges separated by a dash. For [1,3,4,5,7,8,10], return the string "1, 3-5, 7-8, 10".
-
+function bookIndex(array) {
+	var returnString = ""
+	var startPoint = undefined;
+	for (var i = 0; i < array.length - 1; i++) {
+		if (startPoint === undefined) {
+			console.log(startPoint)
+			if (array[i] === array[i + 1] - 1) {
+				console.log('here')
+				startPoint = i;
+			} else {
+				returnString += array[i] + ", ";
+			}
+		} else { //startpoint is defined
+			if (array[i] === array[i + 1] - 1) {
+				//keep going
+				continue;
+			} else {
+				//close it out
+				i++;
+				returnString += array[startPoint] + "-" + array[i-1] + ", ";
+				startPoint = i;
+			}
+		}
+	}
+	console.log('ending out', returnString, startPoint)
+	if (startPoint === undefined) {
+		console.log('last item', array[i])
+		returnString += array[i];
+	} else {
+		if (startPoint === array.length - 1) {
+			returnString += array[startPoint]
+		} else {
+			returnString += array[startPoint] + "-" + array[i];
+		}
+	}
+	return returnString;
+}
+console.log(bookIndex([1, 3, 4, 5, 6, 9, 10]))
 
 // Common Suffix
 // Lance is writing his opus: a tome of beat poetry. Always looking for a good rhyme, he seeks words ending with the same letters. Write a function that, given a word array, returns the largest common suffix (word-end). For inputs ["deforestation", "citation", "conviction", "incarceration"], return "tion" (not a very creative starting point). If it is ["nice", "ice", "baby"], return "".
