8e62a212c095b466778e4daf08b38a924fac14aa
NicMuller <njmuller@outlook.com>
2016-09-01 06:24:15 +0200

    Strings day 5 problem 2 solved

--- Nicholis_Muller/Algorithms/20_Algorithm_Strings_Day5.js
+++ Nicholis_Muller/Algorithms/20_Algorithm_Strings_Day5.js
@@ -1,59 +1,75 @@
 // Here are your challenges:
 // Book Index
 // Martin is writing his opus: a book of algorithm challenges, set as lyrics to a suite of baroque fugues. He knows that some of those fugueing challenges will be less popular than others, so he needs a book index. Given a sorted array of pages, produce a book index string. Consecutive pages should form ranges separated by a dash. For [1,3,4,5,7,8,10], return the string "1, 3-5, 7-8, 10".
-
-function indexString(arr){
+//
+function indexString(arr) {
     var temp = [];
 
     for (var i = 0; i < arr.length; i++) {
 
         var len = arr.length;
         var current = arr[i];
-        var previous = arr[(i+len-1)%len];
-        var next = arr[(i+1)%len];
+        var previous = arr[(i + len - 1) % len];
+        var next = arr[(i + 1) % len];
 
         if (previous !== current - 1) {
             temp.push(current);
-        }else if(previous + 1 === current && current + 1 !== next){
-            temp.push("-"+ current);
+        } else if (previous + 1 === current && current + 1 !== next) {
+            temp.push("-" + current);
         }
     }
 
     var arr = []
 
     for (var i = 0; i < temp.length; i++) {
-        var next = temp[(i+1)%len];
+        var next = temp[(i + 1) % len];
         var current = temp[i];
-        var previous = temp[(i+len-1)%len];
+        var previous = temp[(i + len - 1) % len];
 
 
-        if(typeof current === 'number' && typeof next != 'string'){
+        if (typeof current === 'number' && typeof next != 'string') {
             arr.push(current);
 
-        }else if(typeof current === 'string'){
-            console.log("hit")
+        } else if (typeof current === 'string') {
             arr[i] = (previous.toString() + current)
         }
 
     }
 
-    arr = arr.filter(function(n){ return n != undefined });
+    arr = arr.filter(function(n) {
+        return n != undefined
+    });
     final = arr.join(",");
 
-console.log(final);
+    console.log(final);
 
 }
 
-indexString([1, 3, 4, 5, 7, 8, 10]);
+indexString([1, 3, 4, 5, 7, 8, 10, 13, 14, 15, 19, 23]);
 
 // Common Suffix
 // Lance is writing his opus: a tome of beat poetry. Always looking for a good rhyme, he seeks words ending with the same letters. Write a function that, given a word array, returns the largest common suffix (word-end). For inputs ["deforestation", "citation", "conviction", "incarceration"], return "tion" (not a very creative starting point). If it is ["nice", "ice", "baby"], return "".
 
-// function comSuf(arr){
-//
-// console.log(arr.charAt(-1));
-//
-//
-// }
-//
-// comSuf("deforestation")
+function comSuf(arr) {
+
+    var str = [];
+    var counter = arr.length - 1;
+    var counter2 = -1;
+
+    while (counter >= 0) {
+        for (var i = 0; i < arr.length; i++) {
+            var last = arr[i].length + counter2;
+
+            if (str[str.length - 1] !== arr[i][last]) {
+                str.push(arr[i][last]);
+            }
+        }
+        counter2 -= 1;
+        counter -= 1;
+    }
+
+    var final = str.reverse().join("");
+    console.log(final)
+}
+
+comSuf(["deforestation", "citation", "conviction", "incarceration"])
